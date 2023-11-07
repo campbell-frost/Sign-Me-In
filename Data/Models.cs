f7f30328c57cc7da8b61641e00118d7177415b68
campbell-frost <campbellsfrost@gmail.com>
2023-11-07 13:12:23 -0500

    updated models

--- Data/Models.cs
+++ Data/Models.cs
@@ -75,8 +75,8 @@ public class HubSession
     [Key]
     public int SessionID { get; set; }
     public DateTime Date {get; set;}
-    public TimeSpan TimeIn { get; set; }
-    public TimeSpan? TimeOut { get; set; }
+    public DateTime TimeIn { get; set; }
+    public DateTime? TimeOut { get; set; }
     public string? SessionType {get; set; }
 
     [ForeignKey("Student")]
