b2173ba6c811cfe0d4232dd7ec86db5b865e6a6c
Campbell Frost <campbellsfrost@gmail.com>
2024-02-19 15:18:32 -0500

    Updated DB and removed appsettings

---
+++ Migrations/20240210135156_InitialCreateSQLServer3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sign_Me_In.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateSQLServer3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
